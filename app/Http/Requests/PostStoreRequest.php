39d68b6020303997592851095348228e9e4ef984
pito <pitocms@yahoo.com>
2022-05-09 01:40:00 +0600

    403 error solved

--- app/Http/Requests/PostStoreRequest.php
+++ app/Http/Requests/PostStoreRequest.php
@@ -13,7 +13,7 @@ class PostStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/SubscriptionStoreRequest.php
+++ app/Http/Requests/SubscriptionStoreRequest.php
@@ -13,7 +13,7 @@ class SubscriptionStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
